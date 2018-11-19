d3a550a916123dea7665f7f763684f57aaebc57b
Jayon Kim <jpkim921@gmail.com>
2018-11-19 17:42:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 //   document.getElementsByTagName("main")[0].innerHTML += result;
 // }
 
+// edited using a for loop for several (any) issues 
 function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   for(var i=0;i<issues.length;i++) {
